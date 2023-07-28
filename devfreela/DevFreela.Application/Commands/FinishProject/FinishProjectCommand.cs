44ae10a70d8aab3a79614dcd7c7f449ec5d5b9ec
wharaujo <henrialqui@gmail.com>
2023-07-28 18:08:38 -0300

    Modulo CQRS

---
+++ devfreela/DevFreela.Application/Commands/FinishProject/FinishProjectCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Application.Commands.FinishProject
+{
+    public class FinishProjectCommand : IRequest<Unit>
+    {
+        public FinishProjectCommand(int id) 
+        {
+            Id = id;
+        }
+
+        public int Id { get; private set; }
+    }
+}
---
+++ devfreela/DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using DevFreela.Application.ViewModels;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.GetAllProjects
+{
+    public class GetAllProjectsQueryHandler : IRequestHandler<GetAllProjectsQuery, List<ProjectViewModel>>
+    {
+        private readonly DevFreelaDbContext _dbContext;
+       // private readonly string _connectionString;
+        public GetAllProjectsQueryHandler(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            //_connectionString = configuration.GetConnectionString("DevFreela");
+        }
+        public async Task<List<ProjectViewModel>> Handle(GetAllProjectsQuery request, CancellationToken cancellationToken)
+        {
+            var projects = _dbContext.Projects;
+
+            var projectsViewModel = await projects
+                .Select(p => new ProjectViewModel(p.Id, p.Title, p.CreatedAt))
+                .ToListAsync();
+
+            return projectsViewModel;
+        }
+    }
+}
