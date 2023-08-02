dcc5e420a6bea2b8ef4db3a4acecd7ab2db5f865
wharaujo <henrialqui@gmail.com>
2023-08-02 12:30:18 -0300

    Padrão Repository

---
+++ DevFreela.Application/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
+    {
+        private readonly IProjectRepository _projectRepository;
+        public CreateUserCommandHandler(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User(request.FullName, request.Email, request.BirthDate);
+
+            await _projectRepository.AddUserAsync(user);
+            await _projectRepository.SaveChangesAsync();
+
+            return user.Id;
+        }
+    }
+}
